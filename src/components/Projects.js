acd4889db7474b5882cbcf88acb363353be93a1d
Yangsr-png <JeanIsr05@gmail.com>
2026-02-04 03:39:21 +0100

    Arreglando proyectos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -33,7 +33,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>A continuación presento una selección de mis trabajos más recientes. Desde aplicaciones web completas hasta diseños de interfaces, cada proyecto refleja mi pasión por crear soluciones digitales eficientes y atractivas</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
